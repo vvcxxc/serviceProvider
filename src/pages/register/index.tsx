64ac9cc168fb549aa248019087a9f64d42c12cbc
xiqoqilin <w3c_2014@163.com>
2019-10-15 18:00:08 +0800

    创建注册

---
+++ src/pages/register/index.tsx
@@ -0,0 +1,14 @@
+import React , {Component} from 'react';
+import './index.less';
+
+class Register extends Component {
+   render() {
+       return(
+           <div className="register_wrap">
+               <input type="text"/>
+           </div>
+       )
+   }
+}
+
+export default Register;

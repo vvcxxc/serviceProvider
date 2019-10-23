2a00b8d365a050a7c22bd29639a4cf9707b7c591
xiqoqilin <w3c_2014@163.com>
2019-10-23 09:39:34 +0800

    合并代码

---
+++ src/pages/dataPage/index.tsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import styles from './index.less'
+import { Flex, WingBlank } from 'antd-mobile'
+export default class DataPage extends Component {
+  state = {
+
+  }
+  render (){
+    return (
+      <div className={styles.dataPage}>
+        <Flex className={styles.no_data} justify='around' align='center'>
+          暂无图表数据统计
+        </Flex>
+      </div>
+    )
+  }
+}

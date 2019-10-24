05d3ac3ebdc3315c3dad2bc3e0347b1d16c1670f
xiaoqilin <2597462505@qq.com>
2019-10-24 15:40:37 +0800

    amend:登录页面增加校验

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -17,10 +17,63 @@ export default class PageIndex extends Component {
     mounthTitle: ['一个月', '三个月', '半年', '一年'],
     showMounthTitle: 0,
     inpText: '',
-    inpPassword: ''
+    inpPassword: '',
+    textPrompt: '',
+    passwordPrompt:''
+  }
+
+  delete = () => {
+    this.setState({
+      inpText: ''
+    })
+  }
+
+  //账号输入
+  onChangeText = (value: any) => {
+    // let reg = /^1(3[0-9]|4[5,7]|5[0,1,2,3,5,6,7,8,9]|6[2,5,6,7]|7[0,1,7,8]|8[0-9]|9[1,8,9])\d{8}$/;
+    this.setState({
+      inpText: value
+    }, () => {
+        this.setState({ textPrompt: '' })
+    })
+  }
+  //账号删除
+  onDeleteText = () => {
+    this.setState({
+      inpText: ''
+    })
+  }
+
+  // 密码输入
+  onChangePassword = (value: any) => {
+    this.setState({
+      inpPassword: value
+    }, () => {
+        if (this.state.inpPassword.length < 6) {
+          this.setState({ passwordPrompt: '' })
+          return
+        }
+    })
+  }
+
+  // 密码删除
+  onDeletePassword = () => {
+    this.setState({
+      inpPassword: ''
+    })
   }
 
   landingData = () => {
+    const { inpText, inpPassword } = this.state
+    let reg = /^1(3[0-9]|4[5,7]|5[0,1,2,3,5,6,7,8,9]|6[2,5,6,7]|7[0,1,7,8]|8[0-9]|9[1,8,9])\d{8}$/;
+    if (!reg.test(inpText)) {
+      this.setState({ textPrompt: '请输入正确的手机号码' })
+      return
+    }
+    if (inpPassword.length < 6) {
+      this.setState({ passwordPrompt: '请输入不少于6位数的密码' })
+      return
+    }
     Request({
       url: 'auth/login',
       method: 'post',
@@ -42,46 +95,13 @@ export default class PageIndex extends Component {
     })
   }
 
-  delete = () => {
-    this.setState({
-      inpText: ''
-    })
-  }
-
-  //账号输入
-  onChangeText = (value: any) => {
-    this.setState({
-      inpText: value
-    })
-  }
-  //账号删除
-  onDeleteText = () => {
-    this.setState({
-      inpText: ''
-    })
-  }
-
-  // 密码输入
-  onChangePassword = (value: any) => {
-    this.setState({
-      inpPassword: value
-    })
-  }
-
-  // 密码删除
-  onDeletePassword = () => {
-    this.setState({
-      inpPassword: ''
-    })
-  }
-
   //忘记密码
   forgetPassword = () => {
     router.push({ pathname: '/changePassword/retrieve_password' })
   }
 
   render() {
-    const { day, showMounthTitle } = this.state
+    const {  textPrompt, passwordPrompt } = this.state
     return (
       <div className={styles.pageIndex}>
         <div className={styles.pageContent}>
@@ -93,6 +113,7 @@ export default class PageIndex extends Component {
               onDelete={this.onDeleteText}
               onChange={this.onChangeText}
               mb={56}
+              prompt={textPrompt}
             />
             <MyInput
               placeholder="请输入密码"
@@ -100,6 +121,7 @@ export default class PageIndex extends Component {
               onDelete={this.onDeletePassword}
               onChange={this.onChangePassword}
               mb={26}
+              prompt={passwordPrompt}
             />
             {/* <div className={styles.forgotten} onClick={this.forgetPassword}>忘记密码</div> */}
           </div>

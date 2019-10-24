1dda88f01364503245a9cbced5d0a59df4a3a497
songyizhe <769724661@qq.com>
2019-10-24 13:55:40 +0800

    多环境打包测试

--- umirc.test.ts
+++ umirc.test.ts
@@ -1,7 +1,8 @@
 import { IConfig } from 'umi-types';
 const config: IConfig = {
   define: {
-    "process.env.apiUrl": 'http://test.service_provider_api.tdianyi.com/'
+    "process.env.apiUrl": 'http://test.service_provider_api.tdianyi.com/',
+    "window.api": "http://api.supplier.tdianyi.com/",
   }
 }
 export default config;

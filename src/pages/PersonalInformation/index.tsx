d7293c90e1584072796d4194175f861441756f17
songyizhe <769724661@qq.com>
2019-10-24 17:50:42 +0800

    退出登录

--- src/pages/PersonalInformation/index.tsx
+++ src/pages/PersonalInformation/index.tsx
@@ -33,7 +33,7 @@ export default class PersonalInformation extends Component {
             url: 'auth/logout',
             method: 'post',
         }).then(res => {
-            if (res.code == 200) {
+            if (res.code == 401) {
                 Toast.success('退出登录成功', 1);
                 localStorage.removeItem('token');
                 setTimeout(() => {

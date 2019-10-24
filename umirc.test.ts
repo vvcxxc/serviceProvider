2a009a91ab94921fa3b867440dccd5e9e1667136
songyizhe <769724661@qq.com>
2019-10-24 15:51:29 +0800

    test

--- umirc.test.ts
+++ umirc.test.ts
@@ -2,7 +2,7 @@ import { IConfig } from 'umi-types';
 const config: IConfig = {
   define: {
     "process.env.apiUrl": 'http://test.service_provider_api.tdianyi.com/',
-    "window.api": "http://api.supplier.tdianyi.com/",
+    "API": "http://api.supplier.tdianyi.com/",
   }
 }
 export default config;

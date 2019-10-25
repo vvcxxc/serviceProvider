7d1052bb74bc54fdb668b9a720cb20e944f2cb9b
songyizhe <769724661@qq.com>
2019-10-25 15:52:58 +0800

    文案修改

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,13 +2,11 @@
 import React, { Component } from 'react';
 import styles from './index.less'
 import { Toast, WhiteSpace, WingBlank, Button, Icon } from 'antd-mobile';
-import { success, error } from "../../components/Hint";
 import router from "umi/router";
 import Request from '@/service/request';
 import qs from 'qs';
 import MyInput from "@/components/Input";
 import Green_button from "@/components/Green_button"
-import Calendar from "@/components/Calendar"
 export default class PageIndex extends Component {
 
   state = {
@@ -59,11 +57,11 @@ export default class PageIndex extends Component {
     const { inpText, inpPassword } = this.state
     let reg = /^1(3[0-9]|4[5,7]|5[0,1,2,3,5,6,7,8,9]|6[2,5,6,7]|7[0,1,7,8]|8[0-9]|9[1,8,9])\d{8}$/;
     if (!inpText ) {
-      error('手机号不能为空,请重新输入')
+      Toast.fail('手机号不能为空,请重新输入')
       return
     }
     if (!inpPassword) {
-      error('密码不能为空,请重新输入')
+      Toast.fail('密码不能为空,请重新输入')
       return
     }
 
@@ -76,24 +74,22 @@ export default class PageIndex extends Component {
       })
     }).then(res => {
       const { code, access_token, message, checkout_status } = res
-      if (code !== 200) {//1 首页, 2去修改
-        error('登录失败', message, 2)
-        return
+      if(access_token){
+        localStorage.setItem('token', access_token)
       }
-      localStorage.setItem('token', access_token)
       switch(checkout_status){
         case 0:
-          error('登录失败', '当前服务商账号审核中', 2)
+          Toast.fail('当前服务商账号审核中', 2)
           break
         case 1:
-          success('登录成功')
-          setTimeout(() => {
+          Toast.success('登录成功',1,()=>{
             router.push({ pathname: '/' })
-          }, 1100);
+          })
           break;
         case 2:
-          error('登录失败', '当前服务商账号审核拒绝，请重新提交资料', 2)
-          router.push({ pathname: '/submitQua/BankCard' })
+          Toast.fail('当前服务商账号审核拒绝，请重新提交资料', 2,()=>{
+            router.push({ pathname: '/submitQua/BankCard' })
+          })
           break;
         case 1001:
           router.push('/chooseid')
@@ -102,21 +98,11 @@ export default class PageIndex extends Component {
           router.push('/submitQua/BankCard')
           break
         case 1003:
-          error('登录失败', '账号已禁用，请联系代理商', 2)
+          Toast.fail('账号已禁用，请联系代理商',2)
           break
         default:
           router.push({ pathname: '/' })
       }
-      // if (checkout_status==1) {
-      //   success('登录成功')
-      //   setTimeout(() => {
-      //     router.push({ pathname: '/' })
-      //   }, 1100);
-      // } else {
-      //   setTimeout(() => {
-      //     router.push({ pathname: '/submitQua/BankCard' })
-      //   }, 1100);
-      // }
     })
   }
 

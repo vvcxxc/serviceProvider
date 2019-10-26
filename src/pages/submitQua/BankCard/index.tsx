59371a76435750af6a940033e4ac4258f81a4487
xiqoqilin <w3c_2014@163.com>
2019-10-26 17:42:42 +0800

    修改身份证提示

--- src/pages/submitQua/BankCard/index.tsx
+++ src/pages/submitQua/BankCard/index.tsx
@@ -346,7 +346,7 @@ class BankCard extends Component {
         }
 
         if (!(/^\d{16}|\d{19}$/.test(bankCard))) {
-            Toast.fail('请输入正确的银行卡号', 1);
+            Toast.fail('请输入16-19位的银行卡号', 1);
             return;
         }
 

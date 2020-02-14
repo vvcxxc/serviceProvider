6b42a5eb35a96f4f12292cdd621e47ad89c5ec1c
xiaoqilin <2597462505@qq.com>
2020-02-14 13:49:47 +0800

    feat:新版二维码页面开发

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,13 @@
 /**title: 二维码 */
 import React, { Component } from 'react';
-import styles from './index.less';
-// import router from 'umi/router';
 import Request from '@/service/request';
-import { Toast, PullToRefresh } from 'antd-mobile';
+import { Toast } from 'antd-mobile';
 import { ListCode } from './qr_code_page/component/code'//二维码列表组件
 import { ListPackage } from './qr_code_page/component/package'//列表组码包件
 import { ListStoreQueue } from './qr_code_page/component/store_queue'//铺店队列
 import { ListStreRecord } from './qr_code_page/component/store_record'//铺店记录
 import Filtrate from '../components/Filtrate/ql';//筛选组件
-// import ReactDOM from 'react-dom'
+import styles from './index.less';
 
 export default class QrCodePage extends Component {
   state = {
@@ -17,14 +15,13 @@ export default class QrCodePage extends Component {
     options: ['二维码', '码包', '铺店队列', '铺店记录'],
     options_index: 0,
 
-    currentPrice: 1,
-    is_show_loading: true,
+    currentPrice: 1,//码包单价
 
     //二维码数据
     codeList: [],
     codeTitle: {},
     codePage: 1,
-    codeMore:true,
+    codeMore: true,
 
     //筛选组件
     filter: {},
@@ -73,21 +70,17 @@ export default class QrCodePage extends Component {
   }
 
   componentDidMount() {
-    this.requestList('qrCodes', 1, 1)
-    // this.requestList('Packages', 1, 2)
-    // this.requestList('Attacheds', 1, 3)
-    // this.requestList('LayoutLog', 1, 4)
+    this.requestList(true,'qrCodes', 1, 1)
   }
 
-  requestList = (url?: any, page?: number, options?: number) => {
-    const { options_index, codePage, packagePage, recordPage, codeList, packageList, record_list }
-      = this.state 
-    // const urls = url ? url : ['qrCodes', 'Packages', 'Attacheds', 'LayoutLog'][options_index]//请求地址
-    // const pages = page ? page : [codePage, packagePage, 1, recordPage][options_index] //请求页数
-    // let options_total = options ? options - 1 : options_index  //存在的意义是？
-    const urls =  ['qrCodes', 'Packages', 'Attacheds', 'LayoutLog'][options_index]//请求地址
+  requestList = (ban?:boolean,url?: any, page?: number, options?: number) => {
+    const {
+      options_index, codePage, packagePage, recordPage, queuePage,
+      codeList, packageList, record_list
+    }
+      = this.state
+    const urls = ['qrCodes', 'Packages', 'Attacheds', 'LayoutLog'][options_index]//请求地址
     const pages = [codePage, packagePage, 1, recordPage][options_index] //请求页数
-    // let options_total =  options_index  //存在的意义是？
     let filter = {}
 
     if (!options_index) {
@@ -102,10 +95,11 @@ export default class QrCodePage extends Component {
         ...filter
       }
     }).then(res => {
-      const { data,code } = res
+      const { data, code } = res
       if (code !== 200) return
       switch (options_index) {
         case 0://二维码
+          if (codePage > 1 && !ban) return
           this.setState({
             codeTitle: {
               total: data.qrcode_count,   //共n个码
@@ -113,29 +107,31 @@ export default class QrCodePage extends Component {
               money: data.money_total,    //总收益
             },
             codeList: codePage > 1 ? [...codeList, ...data.data] : data.data,
-            codeMore:data.data.length? true :false
+            codeMore: data.data.length ? true : false
           })
           break;
         case 1://码包
+          if (packagePage > 1 && !ban) return
           this.setState({
-            // packageList: [...this.state.packageList, ...data.data], currentPrice,
-            // packageList: [...this.state.packageList, ...data.data],
+            currentPrice: data.currentPrice,
             packageList: packagePage > 1 ? [...packageList, ...data.data] : data.data,
-            packageMore: data.data.length? true : false
+            packageMore: data.data.length ? true : false
           })
           break;
         case 2://铺店队列
           const { self, Attacheds } = data
+          if (queuePage > 1 && !ban) return
           this.setState({
             queueTitle: self,
             queueList: Attacheds,
-            queueMore: Attacheds.length ? true:false 
+            queueMore: Attacheds.length ? true : false
           })
           break;
         default://铺店记录
+          if (recordPage > 1 && !ban) return
           this.setState({
             record_list: [...record_list, ...data.data],
-            recordMore: data.data.length? true:false
+            recordMore: data.data.length ? true : false
           })
           break;
       }
@@ -145,35 +141,10 @@ export default class QrCodePage extends Component {
     })
   }
 
-
-  // getMoreData = () => {
-  //   if (this.state.is_show_loading) {
-  //     Toast.loading('');
-  //     this.requestList()
-  //   } else {
-  //     Toast.info('暂无更多数据', 1)
-  //   }
-  // }
-
   getOptionsIndex = (options_index: number) => {
-    
-    switch (options_index) {
-      case 0:
-        this.setState({ codePage:  1 })
-        break;
-
-      case 1:
-        this.setState({ packagePage: 1 })
-        break;
-
-      case 3:
-        this.setState({ recordPage:  1 })
-        break;
-
-      default:
-        break;
-    }
-    this.setState({ options_index }, () => { this.requestList() })
+    this.setState({ options_index }
+      , () => { this.requestList() }
+    )
   }
 
   //筛选组触发
@@ -190,27 +161,27 @@ export default class QrCodePage extends Component {
       case 0:
         this.setState({ codePage: this.state.codePage + 1 })
         break;
-      
+
       case 1:
         this.setState({ packagePage: this.state.packagePage + 1 })
         break;
-      
+
       case 3:
         this.setState({ recordPage: this.state.recordPage + 1 })
         break;
-    
+
       default:
         break;
     }
-    this.setState({ options_index: _ }, () => { this.requestList() })
-    
+    this.setState({ options_index: _ }, () => { this.requestList(true) })
+
   }
 
   render() {
     const {
       options_index, options, packageList, currentPrice,
       record_list, queueList, queueTitle, codeList, codeTitle,
-      codeMore, packageMore, queueMore,recordMore
+      codeMore, packageMore, queueMore, recordMore
     } = this.state
     return (
       <div className={styles.qr_code}>
@@ -233,15 +204,11 @@ export default class QrCodePage extends Component {
         }
         {
           [
-            <ListCode list={codeList} title={codeTitle} have_more={codeMore} getWantMore={this.getWantMore}/>,
-            <ListPackage list={packageList} price={currentPrice} have_more={packageMore} getWantMore={this.getWantMore}/>,
-            <ListStoreQueue list={queueList} title={queueTitle} have_more={queueMore} getWantMore={this.getWantMore}/>,
-            <ListStreRecord list={record_list} have_more={recordMore} getWantMore={this.getWantMore}/>][this.state.options_index]
+            <ListCode list={codeList} title={codeTitle} have_more={codeMore} getWantMore={this.getWantMore} />,
+            <ListPackage list={packageList} price={currentPrice} have_more={packageMore} getWantMore={this.getWantMore} />,
+            <ListStoreQueue list={queueList} title={queueTitle} have_more={queueMore} getWantMore={this.getWantMore} />,
+            <ListStreRecord list={record_list} have_more={recordMore} getWantMore={this.getWantMore} />][this.state.options_index]
         }
-        {/* {
-          options_index ? options_index !== 2 ? <div className={styles.more_data_ql} onClick={is_show_loading ? this.scrollBottom : () => { }}>{is_show_loading ? '更多数据' : '暂无更多数据'}</div> : null:null
-          
-        } */}
       </div>
     )
   }
